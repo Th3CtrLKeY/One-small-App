3faad59ea6bb879479b7ae268b0d559452c0dabd
Raghuveer Patil <raghuveerpatil1234@gmail.com>
2023-07-06 15:37:19 +0530

    day_6

---
+++ One_Small_App/day_6/multi-page-webapp/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+const About = () => {
+  return (
+    <div>
+      <h1>SDE BootCamp is a hands-on learning program for people like you.</h1>
+    </div>
+  );
+};
+export default About;
---
+++ One_Small_App/day_6/multi-page-webapp/src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Contact = () => {
+  return (
+    <div>
+      <h1>Mail us on name@gmail.com</h1>
+    </div>
+  );
+};
+export default Contact;
