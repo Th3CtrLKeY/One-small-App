3f79678183f77634de7aeba23c65820a89874850
Raghuveer Patil <raghuveerpatil1234@gmail.com>
2023-07-19 10:39:21 +0530

    day_8

---
+++ One_Small_App/day_8/books-with-hooks/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Search } from "./Search";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Books with Hooks</header>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
